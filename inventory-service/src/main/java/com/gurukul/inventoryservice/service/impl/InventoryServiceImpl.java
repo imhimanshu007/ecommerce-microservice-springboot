18d9adf5c32ad0c40bdf9275abdb0bba9d5cbf04
Himanshu Pandey <himanshupandey2411@gmail.com>
2023-05-31 12:41:24 +0530

    added netflix eureka discovery server

--- inventory-service/src/main/java/com/gurukul/inventoryservice/service/impl/InventoryServiceImpl.java
+++ inventory-service/src/main/java/com/gurukul/inventoryservice/service/impl/InventoryServiceImpl.java
@@ -1,11 +1,13 @@
 package com.gurukul.inventoryservice.service.impl;
 
+import com.gurukul.inventoryservice.dto.InventoryResponse;
 import com.gurukul.inventoryservice.repository.InventoryRepository;
 import com.gurukul.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,7 +17,14 @@ public class InventoryServiceImpl implements InventoryService {
     private final InventoryRepository inventoryRepository;
     @Override
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode)
+                .stream()
+                .map(inventory -> InventoryResponse.builder()
+                        .skuCode(inventory.getSkuCode())
+                        .isInStock(inventory.getQuantity() > 0)
+                        .build()
+                )
+                .toList();
     }
 }
--- order-service/src/main/java/com/gurukul/orderservice/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/gurukul/orderservice/service/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.gurukul.orderservice.service.impl;
 
+import com.gurukul.orderservice.dto.InventoryResponse;
 import com.gurukul.orderservice.dto.OrderLineItemsDto;
 import com.gurukul.orderservice.dto.OrderRequest;
 import com.gurukul.orderservice.model.Order;
@@ -9,7 +10,9 @@ import com.gurukul.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,6 +22,7 @@ import java.util.UUID;
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
     @Override
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -29,7 +33,31 @@ public class OrderServiceImpl implements OrderService {
                 .map(this::mapToEntity)
                 .toList();
         order.setOrderLineItems(orderLineItems);
-        orderRepository.save(order);
+
+        List<String> skuCodes = order.getOrderLineItems()
+                .stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        assert inventoryResponseArray != null;
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+                .allMatch(InventoryResponse::isInStock);
+
+        if(allProductsInStock){
+            orderRepository.save(order);
+        }
+        else{
+            throw new IllegalArgumentException("Product is not in Stock, please try again");
+        }
+
     }
 
     private OrderLineItems mapToEntity(OrderLineItemsDto orderLineItemsDto){
